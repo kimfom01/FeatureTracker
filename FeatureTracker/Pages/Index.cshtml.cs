bf36bd921d09fa7263340262e477926901e7446f
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-11 01:39:18 +0300

    Implement index page to display pending features

--- FeatureTracker/Pages/Index.cshtml.cs
+++ FeatureTracker/Pages/Index.cshtml.cs
@@ -1,19 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FeatureTracker.Data;
+using FeatureTracker.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace FeatureTracker.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly FeatureDbContext _dbContext;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    public IEnumerable<Feature> Features { get; set; } = Enumerable.Empty<Feature>();
+
+    public IndexModel(FeatureDbContext dbContext)
     {
-        _logger = logger;
+        _dbContext = dbContext;
     }
 
-    public void OnGet()
+    public async Task OnGet()
     {
-
+        Features = await _dbContext.Features
+            .Where(f => f.Completed == null)
+            .OrderByDescending(f => f.Priority)
+            .ThenByDescending(f => f.Created)
+            .ToListAsync();
     }
 }
