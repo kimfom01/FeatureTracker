f00aee9606c5b48a8e1450d9cb4785396e228ad0
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-12 17:51:33 +0300

    Replace broken dependencies with new data project reference

--- FeatureTracker/Pages/Features/NewFeature.cshtml.cs
+++ FeatureTracker/Pages/Features/NewFeature.cshtml.cs
@@ -1,6 +1,5 @@
-using FeatureTracker.Data;
-using FeatureTracker.Models;
-using FeatureTracker.Repositories;
+using Data.Models;
+using Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
