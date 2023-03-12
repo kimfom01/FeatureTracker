b22ddecf66bb1dfaf15cfe79a83549926079096b
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-12 18:47:00 +0300

    Add page to display features in a project

---
+++ Pages/Features/ManageFeature.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectManager.Models;
+using ProjectManager.Repositories;
+
+namespace ProjectManager.Pages.Features;
+
+public class ManageFeature : PageModel
+{
+    private readonly IFeatureRepository _featureRepository;
+
+    public ManageFeature(IFeatureRepository featureRepository)
+    {
+        _featureRepository = featureRepository;
+    }
+
+    [BindProperty]
+    public IEnumerable<Feature> Features { get; set; } = Enumerable.Empty<Feature>();
+
+    public void OnGet()
+    {
+        Features = _featureRepository.GetAll()
+            .Where(f => f.Completed == null)
+            .OrderByDescending(f => f.Priority)
+            .ThenByDescending(f => f.Created);
+    }
+}
