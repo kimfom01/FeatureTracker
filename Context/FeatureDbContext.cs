31311613dae44d286cd3cc7a98475a97515700e6
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-12 18:07:39 +0300

    Add tables to context

--- Context/FeatureDbContext.cs
+++ Context/FeatureDbContext.cs
@@ -6,6 +6,8 @@ namespace FeatureTracker.Context;
 public class FeatureDbContext : DbContext
 {
     public DbSet<Feature> Features { get; set; }
+    public DbSet<Project> Project { get; set; }
+    public DbSet<Client> Clients { get; set; }
 
     public FeatureDbContext(DbContextOptions<FeatureDbContext> options) : base(options)
     {
