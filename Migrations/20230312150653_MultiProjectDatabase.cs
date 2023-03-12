6320bccab6e0121d7547c8b9bba49ab9096aec24
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-12 18:08:06 +0300

    Add new migrations for multiple project

---
+++ Migrations/20230312150653_MultiProjectDatabase.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FeatureTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class MultiProjectDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Client_ClientId",
+                table: "Project");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Client",
+                table: "Client");
+
+            migrationBuilder.RenameTable(
+                name: "Client",
+                newName: "Clients");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Clients",
+                table: "Clients",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Clients_ClientId",
+                table: "Project",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Clients_ClientId",
+                table: "Project");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Clients",
+                table: "Clients");
+
+            migrationBuilder.RenameTable(
+                name: "Clients",
+                newName: "Client");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Client",
+                table: "Client",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Client_ClientId",
+                table: "Project",
+                column: "ClientId",
+                principalTable: "Client",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
