7a3bd0bee23cfb805b4bde10cc07b917214bb49e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-12 20:38:36 +0300

    Move save changes to unit of work

--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -5,6 +5,5 @@ public interface IRepository<TEntity> where TEntity : class
     Task AddAsync(TEntity entity);
     IEnumerable<TEntity> GetAll();
     Task<TEntity?> FindAsync(int id);
-    Task<int> SaveChangesAsync();
     Task UpdateAsync(TEntity entity);
 }
